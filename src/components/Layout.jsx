92af41f2b5714223abdad89daa37cfa42f537871
Osama Najjar <osama.najjar.m@gmail.com>
2018-09-14 01:39:54 +0200

    created sidebar section header, body, footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import Sidebar from "./Sidebar";
+import MainView from "./MainView";
+
 
 const Layout = ()=>(
     <div className="layout-wrapper">
         <div className="sidebar-wrapper">
-
+            <Sidebar />
         </div>
         <div className="mainview-wrapper">
-            
+            <MainView />
         </div>
     </div>
 );
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Sidebar = ()=>(
+    <div className="sidebar">
+        <div className="sidebar-header">
+
+        </div>
+        <div className="sidebar-body">
+
+        </div>  
+        <div className="sidebar-footer">
+
+        </div>
+    </div>
+);
+
+export default Sidebar;
