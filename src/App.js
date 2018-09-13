7639d9a9dbe5f5e1260ab799ead6f9940840abd7
Osama Najjar <osama.najjar.m@gmail.com>
2018-09-14 01:02:36 +0200

    created basic layout element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Layout from "./components/Layout";
+import "../node_modules/normalize.scss/normalize.scss";
 import './style.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Layout />
       </div>
     );
   }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Layout = ()=>(
+    <div className="layout-wrapper">
+        <div className="sidebar-wrapper">
+
+        </div>
+        <div className="mainview-wrapper">
+            
+        </div>
+    </div>
+);
+
+export default Layout;
